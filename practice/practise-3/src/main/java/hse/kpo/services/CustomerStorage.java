c69a3ba15ded65b4634c71e6de0d717a52f24cd3
AtomicBiscuit <152898934+atomicbiscuit@users.noreply.github.com>
2025-02-06 22:59:47 +0300

    Rebase complete

--- practice/practise-3/src/main/java/hse/kpo/services/CustomerStorage.java
+++ practice/practise-3/src/main/java/hse/kpo/services/CustomerStorage.java
@@ -2,23 +2,22 @@ package hse.kpo.services;
 
 import hse.kpo.domains.Customer;
 import hse.kpo.interfaces.ICustomerProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- Класс-хранилище пользователей
+ * Класс-хранилище пользователей
  */
 @Component
 public class CustomerStorage implements ICustomerProvider {
-    private List<Customer> customers = new ArrayList<>();
+    private final List<Customer> customers = new ArrayList<>();
 
     /**
-     Возвращает список всех пользователей
-
-     @return список пользователей
+     * Возвращает список всех пользователей
+     *
+     * @return список пользователей
      */
     @Override
     public List<Customer> getCustomers() {
@@ -26,9 +25,9 @@ public class CustomerStorage implements ICustomerProvider {
     }
 
     /**
-     Добавляет пользователя в хранилище
-
-     @param customer пользователь для добавления
+     * Добавляет пользователя в хранилище
+     *
+     * @param customer пользователь для добавления
      */
     public void addCustomer(Customer customer) {
         customers.add(customer); // просто добавляем покупателя в список

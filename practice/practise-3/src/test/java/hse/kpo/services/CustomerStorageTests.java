91a5432e2275b5a056363f2e71892feac6a33a23
AtomicBiscuit <152898934+atomicbiscuit@users.noreply.github.com>
2025-02-06 22:52:21 +0300

    Apply merge develop and sem/4

--- practice/practise-3/src/test/java/hse/kpo/services/CustomerStorageTests.java
+++ practice/practise-3/src/test/java/hse/kpo/services/CustomerStorageTests.java
@@ -13,7 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class CustomerStorageTests {
     @Autowired
     CustomerStorage customerStorage;
--- practice/practise-3/src/test/java/hse/kpo/services/HseCarServiceTests.java
+++ practice/practise-3/src/test/java/hse/kpo/services/HseCarServiceTests.java
@@ -17,7 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class HseCarServiceTests {
     @Autowired
     CarService carService;

0ea3ef38a726c5b3164bf6f408e76e744be167f2
AtomicBiscuit <152898934+atomicbiscuit@users.noreply.github.com>
2025-02-07 21:02:39 +0300

    Complete task for practice-4

--- practice/practise-4/src/main/java/hse/kpo/domains/PedalEngine.java
+++ practice/practise-4/src/main/java/hse/kpo/domains/PedalEngine.java
@@ -1,20 +1,28 @@
 package hse.kpo.domains;
 
-import hse.kpo.interfaces.IEngine;
+import hse.kpo.interfaces.Engine;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+
+/**
+ * Класс, представляющий педальный двигатель.
+ */
 @ToString
 @Getter
-public class PedalEngine implements IEngine {
+@RequiredArgsConstructor
+public class PedalEngine implements Engine {
     private final int size;
 
+    /**
+     * Определяет совместимость покупателя и двигателя.
+     *
+     * @param customer покупатель
+     * @return true, если клиенту хватает силы ног
+     */
     @Override
     public boolean isCompatible(Customer customer) {
         return customer.getLegPower() > 5;
-    }
-
-    public PedalEngine(int size) {
-        this.size = size;
     }
 }

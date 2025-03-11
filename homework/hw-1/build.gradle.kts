af2ef4c4d1cfe13e3c8c179573b1a00270c9613b
AtomicBiscuit <152898934+atomicbiscuit@users.noreply.github.com>
2025-03-11 03:36:17 +0300

    return missing files

---
+++ homework/hw-1/build.gradle.kts
@@ -0,0 +1,77 @@
+plugins {
+    application
+    java
+    checkstyle
+    jacoco
+
+    id("org.springframework.boot") version "3.4.2"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "fm"
+version = "0.0.1-SNAPSHOT"
+
+checkstyle {
+    toolVersion = "10.13.0"
+    isIgnoreFailures = false
+    maxWarnings = 0
+    maxErrors = 0
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+}
+
+application {
+    mainClass.set("App")
+}
+
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+
+    classDirectories.setFrom(
+        files(classDirectories.files.map {
+            fileTree(it) {
+                exclude("**App**")
+            }
+        })
+    )
+}

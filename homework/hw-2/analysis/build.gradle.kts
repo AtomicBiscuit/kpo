5e027d8844197a28b3ec9b21d5f3b70fa3523634
AtomicBiscuit <152898934+atomicbiscuit@users.noreply.github.com>
2025-05-24 19:40:59 +0300

    MVP of hw2

--- homework/hw-2/analysis/build.gradle.kts
+++ homework/hw-2/analysis/build.gradle.kts
@@ -1,20 +1,12 @@
 plugins {
     java
-    checkstyle
     jacoco
-    id("org.springframework.boot") version "3.2.4"
+    id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "hse.kpo"
+group = "hw2"
 version = "1.0-SNAPSHOT"
-
-checkstyle {
-    toolVersion = "10.13.0"
-    isIgnoreFailures = false
-    maxWarnings = 0
-    maxErrors = 0
-}
 
 java {
     toolchain {
@@ -34,18 +26,21 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
 
     runtimeOnly("org.postgresql:postgresql")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
-
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
@@ -55,8 +50,4 @@ tasks.test {
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
-    reports {
-        xml.required = true
-        html.required = true
-    }
 }
--- homework/hw-2/storage/src/main/java/storage/Application.java
+++ homework/hw-2/storage/src/main/java/storage/Application.java
@@ -1,4 +1,4 @@
-package microservice;
+package storage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
